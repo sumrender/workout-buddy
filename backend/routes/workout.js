54f46c5d03f10175d7a333bae1f2953ae130dc4a
sumrender <sumrenders@gmail.com>
2023-01-03 23:14:56 +0530

    adding controllers

--- backend/routes/workout.js
+++ backend/routes/workout.js
@@ -1,24 +1,13 @@
 const express = require("express");
 const router = express.Router();
-const Workout = require("../models/workout");
-
+const {createWorkout} = require("../controllers/workout");
 router.get("/", (req, res) => {
   res.send("get all");
 });
 router.get("/:id", (req, res) => {
   res.send("get one");
 });
-router.post("/", async (req, res) => {
-  const { title, load, reps } = req.body;
-
-  try {
-    const workout = await Workout.create({ title, load, reps });
-    res.status(200).json(workout);
-  } catch (error) {
-    res.status(400).json({ error: error.message });
-  }
-  res.send("post");
-});
+router.post("/", createWorkout);
 router.delete("/:id", (req, res) => {
   res.send("del");
 });
