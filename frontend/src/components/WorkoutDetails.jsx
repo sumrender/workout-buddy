ba4db34f3c45bf5b657eb0ba55cbed19963437b2
sumrender <sumrenders@gmail.com>
2023-01-05 22:52:46 +0530

    app completed

--- frontend/src/components/WorkoutDetails.jsx
+++ frontend/src/components/WorkoutDetails.jsx
@@ -1,4 +1,19 @@
+import { useWorkoutContext } from "../hooks/useWorkoutContext";
+import trashcan from "../assets/trash_icon.png";
 function WorkoutDetails({ workout }) {
+  const { dispatch } = useWorkoutContext();
+
+  const handleClick = async () => {
+    const response = await fetch(
+      "http://localhost:5000/api/workouts/" + workout._id,
+      {
+        method: "DELETE",
+      }
+    );
+    const json = await response.json();
+
+    if (response.ok) dispatch({ type: "DELETE_WORKOUT", payload: json });
+  };
   return (
     <div className="workout-details">
       <h4>{workout.title}</h4>
@@ -11,6 +26,9 @@ function WorkoutDetails({ workout }) {
         {workout.reps}
       </p>
       <p>{workout.createdAt}</p>
+      <span onClick={handleClick}>
+        <img height={20} src={trashcan} alt="delete-icon" />
+      </span>
     </div>
   );
 }
--- frontend/src/components/WorkoutForm.jsx
+++ frontend/src/components/WorkoutForm.jsx
@@ -7,6 +7,7 @@ const WorkoutForm = () => {
   const [load, setLoad] = useState("");
   const [reps, setReps] = useState("");
   const [error, setError] = useState();
+  const [emptyFields, setEmptyFields] = useState([]);
 
   async function handleSubmit(e) {
     e.preventDefault();
@@ -20,12 +21,15 @@ const WorkoutForm = () => {
       },
     });
     const json = await response.json();
-    if (!response.ok) setError(json.error);
-    else {
+    if (!response.ok) {
+      setError(json.error);
+      setEmptyFields(json.emptyFields);
+    } else {
       setTitle("");
       setLoad("");
       setReps("");
       setError(null);
+      setEmptyFields([]);
       console.log("new workout added", json);
       dispatch({ type: "CREATE_WORKOUT", payload: json });
     }
@@ -40,6 +44,7 @@ const WorkoutForm = () => {
         type="text"
         onChange={(e) => setTitle(e.target.value)}
         value={title}
+        className={emptyFields.includes("title") ? "error" : ""}
       />
 
       <label>Load (in kg)</label>
@@ -47,6 +52,7 @@ const WorkoutForm = () => {
         type="number"
         onChange={(e) => setLoad(e.target.value)}
         value={load}
+        className={emptyFields.includes("load") ? "error" : ""}
       />
 
       <label>Reps</label>
@@ -54,6 +60,7 @@ const WorkoutForm = () => {
         type="number"
         onChange={(e) => setReps(e.target.value)}
         value={reps}
+        className={emptyFields.includes("reps") ? "error" : ""}
       />
 
       {error && <div className="error">{error}</div>}
